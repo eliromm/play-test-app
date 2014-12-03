6953e3c34eef1381f6c3dc969604df8c440eafb4
kappa <kappa@kappa-at-clp.local>
2014-12-03 10:03:49 +0900

    元に戻した

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -41,6 +41,7 @@ public class Application extends Controller {
     data.put("user-agent", ua );
     data.put("remote_address", r );
     //
+    //LOG.log("remote-test", data, System.currentTimeMillis()); 時刻表示に注意
     LOG.log("remote-test", data, System.currentTimeMillis());
     return ok(result);
   }
